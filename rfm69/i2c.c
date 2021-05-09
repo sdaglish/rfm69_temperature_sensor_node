cf2d0410c558ee5d7bc7a6416a3c9344ed46cb0e
Steven Daglish <s.c.daglish@gmail.com>
2021-05-09 18:03:33 +0100

    Updated the I2C to correctly output 100kHz
    I also changed the clock setup to make it more clear that this is what
    is happened. This does not affect anything at this stage, thought I
    think this is because I'm actually setting up a lot of the default values.

--- rfm69/i2c.c
+++ rfm69/i2c.c
@@ -1,16 +1,26 @@
 #include "i2c.h"
 
 void i2c_setup(void) {
-  i2c_reset(I2C1);
+  // i2c_reset(I2C1);
   gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9 | GPIO10);
   gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_HIGH, GPIO9 | GPIO10);
   gpio_set_af(GPIOA, GPIO_AF1, GPIO9 | GPIO10);
 
-  i2c_reset(I2C1);
-  i2c_peripheral_disable(I2C1);
+  //  i2c_reset(I2C1);
+  //i2c_peripheral_disable(I2C1);
   
-  i2c_set_speed(I2C1, i2c_speed_sm_100k, 2);  // Actually 2.097MHz, that that doesn't seem to be an option
-  i2c_enable_stretching(I2C1);
+  // The high and low values below had to be hand adjusted to make them work
+  // TODO: I'd like to be able to calculate these values better!
+  //  i2c_set_speed(I2C1, i2c_speed_sm_100k, 2);  // Actually 2.097MHz, that that doesn't seem to be an option
+  i2c_set_prescaler(I2C1, 0);    // 2.097MHz = 476.87ns
+  i2c_set_scl_low_period(I2C1, 7); // 5.0us / 476.87ns = 10.49 ==> 10*476.87ns = 4.77us
+  i2c_set_scl_high_period(I2C1, 6); // 4.0us / 476.87ns = 8.39 ==> 8*476.87ns = 3.81us
+  //i2c_set_scl_low_period(I2C1, 9); // 5.0us / 476.87ns = 10.49 ==> 10*476.87ns = 4.77us
+  //i2c_set_scl_high_period(I2C1, 7); // 4.0us / 476.87ns = 8.39 ==> 8*476.87ns = 3.81us
+  // tSCL ~= 10us ==> 4.77us + 3.81us = 8.58us ==> 8.58us - 10us == 1.42us
+  //i2c_set_data_hold_time(I2C1, 1); //  500ns / 476.87ns = 1.05 ==> 1*476.87ns = 476.87ns
+  //i2c_set_data_setup_time(I2C1, 2); //  500ns / 476.87ns = 1.05 ==> 1*476.87ns = 476.87ns
+  i2c_enable_stretching(I2C1);  // 1250ns / 476.87ns = 2.62
   i2c_set_7bit_addr_mode(I2C1);
   i2c_peripheral_enable(I2C1);
 }
--- rfm69/main.c
+++ rfm69/main.c
@@ -1,3 +1,4 @@
+#include "libopencm3/stm32/l0/memorymap.h"
 #include "libopencm3/stm32/l0/rcc.h"
 #include "libopencm3/stm32/l0/syscfg.h"
 #include <libopencm3/stm32/flash.h>
@@ -20,9 +21,50 @@
 #define FREQUENCY RF69_433MHZ
 
 static void clock_setup(void) {
-  rcc_set_hpre(RCC_CFGR_HPRE_NODIV);
-  rcc_set_ppre1(RCC_CFGR_PPRE1_NODIV);
-  rcc_set_ppre2(RCC_CFGR_PPRE2_NODIV);
+  /* The clock setup below is a modification of the built-in
+     rcc_clock_setup_pll as it doesn't allow use of the MSI
+  */
+
+  struct rcc_clock_scale clock = {
+    .pll_mul = RCC_CFGR_PLLMUL_MUL3,
+    .pll_div = RCC_CFGR_PLLDIV_DIV4,
+    .pll_source = RCC_MSI,
+    .voltage_scale = PWR_SCALE1,
+    .hpre = RCC_CFGR_HPRE_NODIV,
+    .ppre1 = RCC_CFGR_PPRE1_NODIV,
+    .ppre2 = RCC_CFGR_PPRE2_NODIV,
+    .msi_range = 5
+  };
+
+  rcc_set_msi_range(clock.msi_range);
+  rcc_osc_on(RCC_MSI);
+  rcc_wait_for_osc_ready(RCC_MSI);
+  rcc_set_hpre(clock.hpre);
+  rcc_set_ppre1(clock.ppre1);
+  rcc_set_ppre2(clock.ppre2);
+  rcc_periph_clock_enable(RCC_PWR);
+  pwr_set_vos_scale(clock.voltage_scale);
+
+  rcc_osc_off(RCC_PLL);
+  while (rcc_is_osc_ready(RCC_PLL)) {}
+
+  flash_prefetch_enable();
+  flash_set_ws(clock.flash_waitstates);
+
+  /* Set up the PLL */
+  rcc_set_pll_multiplier(clock.pll_mul);
+  rcc_set_pll_divider(clock.pll_div);
+  rcc_set_pll_source(clock.pll_source);
+
+  rcc_osc_on(RCC_MSI);
+  rcc_wait_for_osc_ready(RCC_MSI);
+  rcc_set_sysclk_source(RCC_MSI);
+
+  /* Set the peripheral clock frequencies used. */
+  // These currently aren't actually being used and could probably be removed without any issues.
+  rcc_ahb_frequency = clock.ahb_frequency;
+  rcc_apb1_frequency = clock.apb1_frequency;
+  rcc_apb2_frequency = clock.apb2_frequency;
 
   // GPIO
   // rcc_periph_clock_enable(RCC_GPIOB);
@@ -33,8 +75,10 @@ static void clock_setup(void) {
   rcc_periph_clock_enable(RCC_SPI1);
 
   // I2C
+  rcc_set_peripheral_clk_sel(I2C1_BASE, 0b01);
+  //RCC_CCIPR |= (1 << 12);
   rcc_periph_clock_enable(RCC_I2C1);
-}
+					  }
 
 static char *itoa(int value, char *result, int base) {
   // check that the base if valid
@@ -226,7 +270,7 @@ int main(void) {
     rfm69_sendWithRetry(GATEWAYID, payload, sendSize, 20, 200);
 
     uint32_t now = timer_get_sys_tick();
-    while ((timer_get_sys_tick() - now) < 30000) {
+    while ((timer_get_sys_tick() - now) < 1000) {
     }
   }
 }
